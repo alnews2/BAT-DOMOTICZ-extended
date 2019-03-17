cac9d5ab5d1c0005986e6b7f06c5f58f6534025f
alnews2 <alnews2@aliceadsl.fr>
2019-03-17 10:53:13 +0100

    ajout mode heat sur changement pout maj état

--- ZXT1Max.bat
+++ ZXT1Max.bat
@@ -9,7 +9,11 @@ for /f "delims=" %%i in ('date/T') do Set MyDate=%%i
 for /f "delims=" %%i in ('time/T') do Set Mytime=%%i
 echo %MyDate% : %Mytime% (%3 %1 secondes) >>%logfile%
 echo [--------------------------] >>%logfile%
+call ZXTxMODEy %ZXT1mode% %Modeheat%
+timeout 1 >NUL
 call ZXTxSETTEMPy %ZXT1heatTemp% 29
+timeout 1 >NUL
 call ZXTxfany %ZXT1fan% %FanOnHigh%
+timeout 1 >NUL
 echo [===================ZXT1Max]] >>%logfile%
 
--- ZXT3Max.bat
+++ ZXT3Max.bat
@@ -9,7 +9,11 @@ for /f "delims=" %%i in ('date/T') do Set MyDate=%%i
 for /f "delims=" %%i in ('time/T') do Set Mytime=%%i
 echo %MyDate% : %Mytime% (%3 %1 secondes) >>%logfile%
 echo [--------------------------] >>%logfile%
+call ZXTxMODEy %ZXT3mode% %Modeheat%
+timeout 1 >NUL
 call ZXTxSETTEMPy %ZXT3heatTemp% 29
+timeout 1 >NUL
 call ZXTxfany %ZXT3fan% %FanOnHigh%
+timeout 1 >NUL
 echo [=====================ZXT3Max]] >>%logfile%
 
